5bd590c84711bff03d810047ef7114905267353d
Chuck Allen <ChuckAllen@QuickenLoans.com>
2020-09-03 17:21:16 -0400

    Added in animation of ship image to the form, animation is not too distracting while adding to the theme of the site

--- IntergalacticDrinks/wwwroot/js/site.js
+++ IntergalacticDrinks/wwwroot/js/site.js
@@ -2,3 +2,59 @@
 // for details on configuring this project to bundle and minify static web assets.
 
 // Write your JavaScript code.
+
+
+var canvas;
+var ctx;
+var desiredInterval = 100;
+var height;
+var width;
+var shipImg;
+var shipX;
+var shipY;
+var shipDirectional;
+
+function updateSize() {
+    //todo: adjust canvas size to size of document body.
+    var oldWidth = width;
+    height = Math.max($(document).height(), $(window).height());
+    width = Math.max($(document).width(), $(window).width());
+    shipX = shipX + ((width - oldWidth)/2);
+    canvas.setAttribute("width", width);
+    canvas.setAttribute("height", height);
+}
+
+function updateBackground() {
+    document.body.style.background = "url(" + canvas.toDataURL() + ") no-repeat fixed center center /cover";
+}
+
+function updateAnimation() {
+    //todo: update locations and draw
+    if (Math.floor(Math.random() * 10) < 2) {
+        shipDirectional = -1 * shipDirectional;
+    }
+    shipX = shipX + shipDirectional;
+    ctx.drawImage(shipImg, shipX, shipY, shipImg.width, shipImg.height);
+}
+
+function prepareAnimation() {
+    shipImg = document.getElementById("shipImage");;
+    canvas = document.getElementById("background");
+    ctx = canvas.getContext("2d");
+    desiredInterval = 100;
+    height = Math.max($(document).height(), $(window).height());
+    width = Math.max($(document).width(), $(window).width());
+    shipX = ((width / 2) - (shipImg.width / 2));
+    shipY = (height - shipImg.height);
+    shipDirectional = 3;
+}
+
+//occurs when the document is ready, calls setInterval to make the updates happen every so often
+$(document).ready(function () {
+    prepareAnimation();
+    setInterval(() => {
+        updateSize();
+        updateAnimation();
+        updateBackground();
+    }, desiredInterval);
+});
